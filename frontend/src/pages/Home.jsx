234c61c48cfe85bed2c248bcd36368971ca36798
Muzzammil Afzal <muzzu2605afzall@gmail.com>
2025-11-08 11:36:37 +0530

    merge conflict

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,22 +1,11 @@
 import React from 'react'
-<<<<<<< HEAD
-import Navbar from './NavLogin'
-import NavLogin from './NavLogin'
-import Login from './Login'
-import SignUp from './SignUp'
-=======
 import Loading from '../components/loading'
->>>>>>> aa71ad057e4ca656855373d6f0ce9b4c0004b4a6
 
 const Home = () => {
   return (
     <div>
       
-<<<<<<< HEAD
-      HOME
-=======
       
->>>>>>> aa71ad057e4ca656855373d6f0ce9b4c0004b4a6
     </div>
   )
 }
